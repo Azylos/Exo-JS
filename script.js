4e8c4c66edbe542a2c1183ae33e4e98c00f8eff5
Toma <tomabarret@outlook.fr>
2024-10-23 11:08:57 +0400

    ajout bonus

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ function change(){
     document.getElementById('btn').innerHTML = document.getElementById('text').value
     document.getElementById('text').value = '';
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('text').addEventListener('input', function() {
+        document.getElementById('btn').innerHTML = this.value;
+    });
+});
